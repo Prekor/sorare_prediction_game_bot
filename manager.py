be52ba85f8f3275f451be2941c3c53e2b6e6346e
Prekor <prekorista@gmail.com>
2022-02-01 18:58:33 +0100

    Changed class attribute name

--- manager.py
+++ manager.py
@@ -1,6 +1,6 @@
 
 
 class Manager:
-    def __init__(self, name: str):
-        self.name = name
+    def __init__(self, name):
+        self.author = name
         # what about same user that has 2 discord accounts?
